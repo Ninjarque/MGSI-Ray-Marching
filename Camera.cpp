a24f2da7ac7103f2b4a85d1439f35e5b962b8bd6
belloaltine5279 <32312772+belloaltine5279@users.noreply.github.com>
2023-01-03 22:44:25 +0100

    Update Camera.cpp

--- Camera.cpp
+++ Camera.cpp
@@ -38,46 +38,46 @@ void Camera::setFieldOfView(const vec2 &a) {
     flag = true ;
 }
 
-void Camera::setPosition(const vec3 &a) {
+void Camera::setPosition(const vec3 &a) { // modification de la position de la camera 
     this->position = a ;
-    flag = true ;
+    flag = true ; // pour declancher que la camera a été bouger
 }
 
-void Camera::setRotation(const vec3 &a) {
+void Camera::setRotation(const vec3 &a) { // permet la rotation de la camera 
     this->rotation = a ;
     flag = true ;
 }
 
-void Camera::move(const vec3 &m){
+void Camera::move(const vec3 &m){ // pour bouger la camera selon un vecteur donnée en paramètre 
     this->position += m ;
     this->flag = true ; // pour declancher que la camera a ete bouger
 }
 
-void Camera::rotateX(const float &amount){
+void Camera::rotateX(const float &amount){ // changer la rotation en x 
     this->rotation.x += amount;
-    if (amount != 0.0f) this->flag = true;
+    if (amount != 0.0f) this->flag = true; // declancher que la camera été bouger 
 }
-void Camera::rotateY(const float &amount){
+void Camera::rotateY(const float &amount){ // changer la rotation en y 
     this->rotation.y += amount;
-    if (amount != 0.0f) this->flag = true;
+    if (amount != 0.0f) this->flag = true; // declancher que la camera été bouger 
 }
-void Camera::rotateZ(const float &amount){
+void Camera::rotateZ(const float &amount){ // changer la rotation en z 
     this->rotation.z += amount;
-    if (amount != 0.0f) this->flag = true;
+    if (amount != 0.0f) this->flag = true; // declancher que la camera été bouger 
 }
 
 vec2 Camera::getFieldOfView() const {
     return this->fieldOfView ; // avoir le champ de vision de la camera
 }
 
-vec3 Camera::getPosition() const {
+vec3 Camera::getPosition() const { // avoir la position de la camera
     return this->position;
 }
-vec3 Camera::getRotation() const {
+vec3 Camera::getRotation() const { // avoir la rotation de la camera 
     return this->rotation;
 }
 
-mat4 Camera::getRotationMatrix() const {
+mat4 Camera::getRotationMatrix() const { // avoir la rotation matrice de la camera 
     vec3 rotation = getRotation();
     mat4 rotationMatrix(1.0);
     rotationMatrix = rotate(mat4(1.0f), rotation.z, vec3(0.0f, 0.0f, 1.0f)) *
@@ -86,17 +86,17 @@ mat4 Camera::getRotationMatrix() const {
     return rotationMatrix;
 }
 
-vec3 Camera::getForward() const {
+vec3 Camera::getForward() const { // avoir l'avant de la camera 
     vec4 forward(0.0f, 0.0f, 1.0f, 1.0f);
     forward = getRotationMatrix() * forward;
     return vec3(forward.x, forward.y, forward.z);
 }
-vec3 Camera::getRight() const {
+vec3 Camera::getRight() const { // avoir la droite de la camera 
     vec4 right(1.0f, 0.0f, 0.0f, 1.0f);
     right = getRotationMatrix() * right;
     return vec3(right.x, right.y, right.z);
 }
-vec3 Camera::getUp() const {
+vec3 Camera::getUp() const { // avoir le haut de la camera 
     vec4 up(0.0f, 1.0f, 0.0f, 1.0f);
     up = getRotationMatrix() * up;
     return vec3(up.x, up.y, up.z);
