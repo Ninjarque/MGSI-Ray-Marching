a38297388a227355d3984efb085ed5f281fff8f2
belloaltine5279 <32312772+belloaltine5279@users.noreply.github.com>
2023-01-04 18:59:06 +0100

    Update Camera.h

--- Camera.h
+++ Camera.h
@@ -23,17 +23,15 @@ public:
 int WIDTH = 800;
     Camera(const vec3 &, const vec3 &, const vec2 &);
     ~Camera() ;
-
-
     void setFieldOfView(const vec2 &);
     void setRotation(const vec3 &);
     void setPosition(const vec3 &);
     void moveCamera(const vec3 & ) ;
-
+    
     void rotateX(const float &);
     void rotateY(const float &);
     void rotateZ(const float &);
-
+    
     vec3 getPosition() const; 
     vec2 getFieldOfView() const ; 
     vec3 getRotation() const ;
@@ -43,11 +41,10 @@ int WIDTH = 800;
     vec3 getUp() const;
     
     void move(const vec3 &) ; 
-
+    
     bool isMoved(){
         return flag ;
     }
-
     void moveResolved() { flag = false; }
 };
 
