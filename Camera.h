169afba8b9280a3848a44797bf0bca8947d03754
belloaltine5279 <32312772+belloaltine5279@users.noreply.github.com>
2023-01-04 18:58:16 +0100

    Update Camera.h

--- Camera.h
+++ Camera.h
@@ -16,19 +16,14 @@ class Camera{
 private :
     bool flag;
     vec3 position;
-    //vec3 lookAt;
-    //vec3 cameraHaut;
-    vec3 rotation; // x et y cameraanglex et cameraangley
-    //vec3 translation; // x et y et cameraDistance
+    vec3 rotation; // x et y cameraanglex et cameraangley // x et y et cameraDistance
     vec2 fieldOfView;
 public:
     Camera();
 int WIDTH = 800;
-    Camera(const vec3 &, const vec3 &, const vec2 &); //, const vec3 &, const vec3 &) ;
+    Camera(const vec3 &, const vec3 &, const vec2 &);
     ~Camera() ;
 
-    //void setLookAt(const vec3&) ;
-    //void setUp(const vec3&) ;
 
     void setFieldOfView(const vec2 &);
     void setRotation(const vec3 &);
@@ -42,10 +37,7 @@ int WIDTH = 800;
     vec3 getPosition() const; 
     vec2 getFieldOfView() const ; 
     vec3 getRotation() const ;
-    //vec3 getTranslation() const ;
-
     mat4 getRotationMatrix() const;
-
     vec3 getForward() const;
     vec3 getRight() const;
     vec3 getUp() const;
